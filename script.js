36fdd6cfe536d5a1b5ceb69f7926c9f0e369fd5b
vincev20 <vincev20@yahoo.com>
2016-10-08 23:45:26 +0800

    update
    update

--- script.js
+++ script.js
@@ -3,9 +3,10 @@
 var elem = "<div style=display: inline-block; border: 0px solid white;'><img style='width:75px;height:75px;margin: 0px;padding-top: 20px' src='images/blackgrey.jpg'></div><div style='display: inline-block; border: 0px solid white;'><div style=' border: 0px solid white;'>Soul Lace</div><div style='display: inline-block; border: 1px solid white;'><div style='display: inline-block; border: 1px solid white;'>&nbsp-&nbsp</div><div style='display: inline-block; border: 1px solid white;'>&nbsp&nbsp1&nbsp&nbsp</div><div style='display: inline-block; border: 1px solid white;'>&nbsp+&nbsp</div></div><div style='display: inline-block; border: 0px solid white;'>&nbsp&nbsp&nbsp$19.00</div></div><div style='border: 0px solid white;'>&nbsp</div>"
 
 
-var $pictures = ["bluegrey.jpg", "limegrey.jpg","mintgrey.jpg","mustardgrey.jpg","purplegrey.jpg","redblack.jpg","whiteblack.jpg","whitegrey.jpg"]
-var $names = ["Blue", "Black","Olive","Teal","Purple","Red","White","Gray"]
-var $prices = ["10", "20","30","40","50","60","70","80"]
+var $pictures = ["bluegrey.jpg", "blackgrey.jpg","limegrey.jpg","mintgrey.jpg","purplegrey.jpg","mustardgrey.jpg","redblack.jpg","whiteblack.jpg","whitegrey.jpg"]
+var $names = ["Blue", "Black","Olive","Teal","Purple","Orange","Red","White","Gray"]
+var $prices = [10,20,30,40,50,60,70,80,90]
+var quantities = [0,0,0,0,0,0,0,0,0]
  
 var $nextLink = $('#nextLink')
 var $prevLink = $('#prevLink')
@@ -14,6 +15,8 @@ var $colorButtons = $('.colored')
 var $name = $('#name')
 var $price = $('#price')
 var $items = $('#items')
+
+var totalPrice = 0;
 
 $colorButtons.click(function(){
 
@@ -65,14 +68,67 @@ $addToCartButton.click(function() {
 	*/
 
 
+	var link = $mainPicture.attr("src"); 
+	var index;
+
+	switch (link){
+			case "images/bluegrey.jpg":
+				index = 0;
+			break;
+
+			case "images/blackgrey.jpg":
+				index = 1;
+			break;
+
+			case "images/limegrey.jpg":
+				index = 2;
+			break;
+
+			case "images/mintgrey.jpg":
+				index = 3;
+			break;
+
+			case "images/purplegrey.jpg":
+				index = 4;
+			break;
+
+			case "images/mustardgrey.jpg":
+				index = 5;
+			break;
+
+			case "images/redblack.jpg":
+				index = 6;
+			break;
+
+			case "images/whiteblack.jpg":
+				index = 7;
+			break;
+
+			case "images/whitegrey.jpg":
+				index = 8;
+			break;
+
+	}
+
+	console.log(index);
 
 
+	if (quantities[index]==0){
+		$("ul#items li:eq(" +index + ")").toggle();
 
-	//var link = $mainPicture.attr("src"); 
-	// alert($link)
+	}
 
-	//$items.append("<li><div class='itemContainer'><img style='display:inline-block' src=" + link + " /><p style='display:inline-block'>Soul Laces</p><p>- 1 + $19.00</p></div></li>");
-	$items.append(elem);
+	quantities[index]++;
+
+	$("ul#items .listName:eq(" +index + ")").html($names[index] + " ");
+
+	totalPrice = +totalPrice + $prices[index]
+	
+	$('.quantityInfo:eq(' + index + ') ').html(quantities[index])
+	$("#panelTotalPrice").html(totalPrice);
+
+
+	$('.priceBoxSpan:eq(' + index + ') ').html($prices[index]);
 
 	$(".panel").toggleClass('open');
 	//$(this).toggleClass("active");
@@ -80,13 +136,41 @@ $addToCartButton.click(function() {
  
 })
 
+$(".plusButton").click(function(){
+	var currentIndex2 = $('.plusButton').index(this);
+	
+	quantities[currentIndex2] = quantities[currentIndex2] - 1
+	$('.quantityInfo:eq(' + currentIndex2 + ') ').html(quantities[currentIndex2])
 
+	if (quantities[currentIndex2]==0){
+		$("ul#items li:eq(" + currentIndex2 + ")").toggle();
 
+	}else{
 
+		
+		
+	}
+
+	totalPrice = +totalPrice - $prices[currentIndex2]
+	$("#panelTotalPrice").html(totalPrice);
+
+	console.log(currentIndex2)
+})
+
+$(".minusButton").click(function(){
+	var currentIndex2 = $('.minusButton').index(this);
+	totalPrice = +totalPrice + $prices[currentIndex2]
+	
+	quantities[currentIndex2] = quantities[currentIndex2] + 1
+	$('.quantityInfo:eq(' + currentIndex2 + ') ').html(quantities[currentIndex2])
+	$("#panelTotalPrice").html(totalPrice);
+	console.log(currentIndex2)
+})
 
 
 $('#cartButton').click(function() {
 	$(".panel").toggleClass('open');
+
 	//$(this).toggleClass("active");
 	return false;
  
